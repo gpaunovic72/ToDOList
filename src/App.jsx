3c2c86901297f651d3192c88b65280a76faf95c8
Goran Paunovic <g.paunovic.8@gmail.com>
2025-01-28 15:28:40 +0100

    Mise en place du style et des fonctionnalités de la todolist

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import "./App.css";
 import ToDoList from "./components/ToDoList";
 
 function App() {
-  return <ToDoList title="ToDoList" />;
+  return (
+    <div className="app">
+      <ToDoList title="ToDoList" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,10 +1,55 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import "../ToDoList/ToDoList.scss";
 
 export default function ToDoList({ title }) {
+  const [task, setTask] = useState([]);
+  const [saisie, setSaisie] = useState("");
+
+  const addTask = () => {
+    if (saisie) {
+      setTask([...task, { text: saisie, completed: false }]);
+      setSaisie("");
+    }
+  };
+
+  const toggleTask = (index) => {
+    const updateTask = task.map((tasks, i) =>
+      i === index ? { ...tasks, completed: !tasks.completed } : tasks
+    );
+    setTask(updateTask);
+  };
+
   return (
     <div className="todolist">
-      <h1>{title}</h1>
+      <h1 className="todolist__title">{title}</h1>
+      <div className="todolist__search">
+        <input
+          type="text"
+          value={saisie}
+          onChange={(e) => setSaisie(e.target.value)}
+          className="todolist__search--saisie"
+        />
+        <button onClick={addTask} className="todolist__search--btn">
+          Ajouter
+        </button>
+      </div>
+      <ul className="todolist__list">
+        {task.map((tasks, index) => (
+          <li key={index} className="todolist__list--item">
+            <label>
+              <input
+                type="checkbox"
+                checked={tasks.completed}
+                onChange={() => toggleTask(index)}
+              />
+              <span className={tasks.completed ? "completed" : ""}>
+                {tasks.text}
+              </span>
+            </label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
